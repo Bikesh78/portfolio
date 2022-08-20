e1b4f19313ba4482b2cd91a79d76adc5816d5b54
Bikesh Rai <bikeshrai78@gmail.com>
2022-08-20 18:44:26 +0545

    Add animation

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const body = document.querySelector("body");
 mobileMenu.addEventListener("click", (e) => {
   body.classList.toggle("show-mobile-menu");
 });
+
+function animate() {
+  const elements = document.querySelectorAll(".animate");
+  elements.forEach((element) => {
+    let windowHeight = window.innerHeight;
+    const positionFromTop = element.getBoundingClientRect().top;
+    let elementVisible = 150;
+    if (positionFromTop < windowHeight - elementVisible) {
+      element.classList.add("active");
+    } else {
+      element.classList.remove("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", animate);
